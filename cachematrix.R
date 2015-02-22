9c435510100fb9feec5f1d4e14d2b7ef3cf1456a
Christophe Chevalier <Kristoff@MacBook-Pro.home>
2015-02-22 12:37:53 +0100

    final version

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,56 @@
 ## This R script contains a pair of functions that cache the inverse of a matrix.
 ## 
 ## Programming Assignment 2 for R Programming course on coursera.org
-## February 2014
+## February 2015
+##
+##
+## Usage:
+##
+##  > m <- makeCacheMatrix(rbind(c(2, 0), c(0, 2)))
+##  > m$getinv()
+##  NULL
+##
+##  > cacheSolve(m)
+##       [,1] [,2]
+##  [1,]  0.5  0.0
+##  [2,]  0.0  0.5
+##
+##  > cacheSolve(m)
+##  returning the cached inverse matrix
+##       [,1] [,2]
+##  [1,]  0.5  0.0
+##  [2,]  0.0  0.5
+##
+##  > m$getinv()
+##       [,1] [,2]
+##  [1,]  0.5  0.0
+##  [2,]  0.0  0.5
+##
+
 
 ## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
+  
+  # we originally cache the inverse matrix minv as NULL
   minv <- NULL
+  
+  # Set function for the matrix
   set <- function(y) {
     x <<- y
+    # reset minv to null
     minv <<- NULL
   }
+  
+  # get function for the matrix
   get <- function() x
+  
+  # set function for the inverse matrix
   setinv <- function(inv) minv <<- inv
+  
+  # get function for the inverse matrix
   getinv <- function() minv
+  
+  # Package all functions into a list
   list(set = set, get = get,
        setinv = setinv,
        getinv = getinv)
@@ -24,11 +62,17 @@ makeCacheMatrix <- function(x = matrix()) {
 ## then the cachesolve should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  # First retrieve the cached inverse matrix
   minv <- x$getinv()
-  if(!is.null(minv)) {
-    message("getting cached data")
+  
+  # If minv is not null then return the cached value as the inverse matrix
+  if(!is.null(minv)) {  
+    message("returning the cached inverse matrix")
     return(minv)
   }
+  
+  # Compute the inverse matrix using solve() then cache the result and finally return it
   data <- x$get()
   minv <- solve(data, ...)
   x$setinv(minv)
